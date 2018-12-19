63b08a3940c4515807bdb18c1fecd68f1add7700
Luke <baslilonfive@gmail.com>
2018-12-19 16:48:26 -0600

    Initial Interaction(except some details and pics animations)

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -77,7 +77,7 @@ export class MainComponent implements OnInit {
   };
   document.getElementById('logo-nav-box').addEventListener('animationend', () => {
       document.getElementById('header-title').style.display = 'block';
-      document.getElementById('header-title').className = 'title animated fadeInLeft';
+      document.getElementById('header-title').className = 'title animated fadeInRight';
   });
   setTimeout(() => {
     document.getElementById('main-header').style.display = 'block';
@@ -86,8 +86,11 @@ export class MainComponent implements OnInit {
 document.getElementById('header-title').addEventListener('animationend', () => {
     document.getElementById('header-subtitle').style.display = 'block';
     document.getElementById('header-subtitle').className = 'subtitle animated fadeInRight';
-
 });
+// document.getElementById('case-studies-section').addEventListener('animationend', () => {
+//   document.getElementById('binaryvision').style.display = 'block';
+//   document.getElementById('binaryvision').className = 'binaryvision animated fadeInLeft';
+// });
 
 
   window.addEventListener('scroll',()=>{
@@ -104,16 +107,93 @@ document.getElementById('header-title').addEventListener('animationend', () => {
 
 
     const caseStudiesSection = document.getElementById('case-studies-section').offsetTop;
+    const binaryvisionSection = document.getElementById('case-studies-section binaryvision').offsetTop -  (window.innerHeight * 14 / 100) -
+    (window.innerHeight * 12 / 100) * 2;
     const caseStudiesSectionEnd = document.getElementById('case-studies-section').offsetTop +
         document.getElementById('case-studies-section').offsetHeight;
+    
+    const binaryvisionDevelopment = document.getElementById('case-studies-section').offsetTop - (window.innerHeight * 14 / 100) -
+    (window.innerHeight * 12 / 100) * 2;
+  
+    
     if (top >= caseStudiesSection && top <= caseStudiesSectionEnd) {
         document.getElementById('logo-image').setAttribute('src', '../../assets/Services/Group 4.png');
     } else {
         document.getElementById('logo-image').setAttribute('src', '../../assets/Services/logo.png');
     }
+    //Binary Vision content
+    if (top>=binaryvisionSection){
+      document.getElementById('binaryvision').style.display = 'block';
+      document.getElementById('binaryvision').className = 'binaryvision animated fadeInLeft';
+      document.getElementById('binaryvision').addEventListener("animationend",()=>{
+        document.getElementById('content').style.display = 'block';
+        document.getElementById('content').className = 'content animated fadeInUp';
+      });
+    
+    //binaryvision-development
+    if(top>=binaryvisionDevelopment){
+      document.getElementById('binaryvision-development-pic1').style.display="block";
+      document.getElementById('binaryvision-development-pic1').className='binaryvision-development-pic animated fadeInRight'; 
+      document.getElementById('background1').style.opacity="1";
+      document.getElementById('background1').className='background1 animated fadeInLeft'; 
+      document.getElementById('development-brand-content').style.display="block";
+      document.getElementById('development-brand-content').className='development-brand-content animated fadeInUp'; 
+      document.getElementById('development-services').style.display="block";
+      document.getElementById('development-services').className='development-services animated fadeInUp';
+      // document.getElementById('development-brand-span').style.display="block";
+      // document.getElementById('development-brand-span').className='development-brand-span animated fadeInUp'; 
+    }
+    const binaryvisionUi = document.getElementById('case-studies-section').offsetTop + innerHeight*1.3 - (window.innerHeight * 14 / 100) -
+    (window.innerHeight * 12 / 100) * 2;
+
+    if(top>=binaryvisionUi){
+    document.getElementById('binaryvision-uiux-pic-box').style.display="block";
+    document.getElementById('binaryvision-uiux-pic-box').className='binaryvision-uiux-pic-box animated fadeInLeft'; 
+    document.getElementById('background2').style.opacity="1";
+    document.getElementById('background2').className='background2 animated fadeInRight';
+    document.getElementById('ui-brand-content').style.display="block";
+    document.getElementById('ui-brand-content').className='ui-brand-content animated fadeInUp'; 
+    document.getElementById('ui-services').style.display="block";
+    document.getElementById('ui-services').className='ui-services animated fadeInUp';
+    }
+
+    const binaryvisionDigita = document.getElementById('case-studies-section').offsetTop + innerHeight*3 - (window.innerHeight * 14 / 100) -
+    (window.innerHeight * 12 / 100) * 2;
+
+    if(top>=binaryvisionDigita){
+        document.getElementById('binaryvision-digital-pic1').style.display="block";
+        document.getElementById('binaryvision-digital-pic1').className='pic1 animated fadeInRight'; 
+        document.getElementById('binaryvision-digital-pic2').style.display="block";
+        document.getElementById('binaryvision-digital-pic2').className='pic2 animated fadeInRight';
+        document.getElementById('backgroud3').style.opacity="1";
+        document.getElementById('backgroud3').className='backgroud3 animated fadeInLeft';
+        document.getElementById('digital-brand-content').style.display="block";
+        document.getElementById('digital-brand-content').className='digital-brand-content animated fadeInUp'; 
+        document.getElementById('digital-services').style.display="block";
+        document.getElementById('digital-services').className='digital-services animated fadeInUp';
+        }
+
+    const binaryvisionBrand = document.getElementById('case-studies-section').offsetTop + innerHeight*4.8 - (window.innerHeight * 14 / 100) -
+    (window.innerHeight * 12 / 100) * 2;
+    
+    if(top>=binaryvisionBrand){
+        document.getElementById('binaryvision-brand-box1').style.display="block";
+        document.getElementById('binaryvision-brand-box1').className='box1 animated fadeInLeft'; 
+        document.getElementById('binaryvision-brand-box2').style.display="block";
+        document.getElementById('binaryvision-brand-box2').className='box2 animated fadeInLeft';
+        document.getElementById('backgroud4').style.opacity="1";
+        document.getElementById('backgroud4').className='backgroud4 animated fadeInRight';
+        document.getElementById('brand-brand-content').style.display="block";
+        document.getElementById('brand-brand-content').className='brand-brand-content animated fadeInUp'; 
+        document.getElementById('brand-services').style.display="block";
+        document.getElementById('brand-services').className='brand-services animated fadeInUp';
+        }
+
+    }
     if (top > caseStudiesSection) {
         document.getElementById('out-of-thought-section').style.display = 'none';
         document.getElementById('footer').style.display = 'flex';
+
     } else {
         document.getElementById('out-of-thought-section').style.display = 'flex';
         document.getElementById('footer').style.display = 'none';
